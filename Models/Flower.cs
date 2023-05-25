a9defaf37190deb16524eee4220bc8ec209d65ab
Wael <73485104+WaelWM@users.noreply.github.com>
2023-05-25 22:42:45 +0800

    Add files via upload

---
+++ Models/Flower.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCFlowerShopWeek3.Models
+{
+    public class Flower
+    {
+        [Key]// Primary key
+        public int FlowerId { get; set; }
+
+        public string FlowerName { get; set; }
+
+        public string FlowerType { get; set;}
+
+        public decimal FlowerPrice { get; set;}
+
+        public DateTime FlowerPurchasedDate { get; set;}
+
+
+    }
+}
